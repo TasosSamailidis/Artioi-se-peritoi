873bc4f48a52e9f46bed806fe8627c08bb800b3a
TasosSama <48088210+TasosSamailidis@users.noreply.github.com>
2019-03-07 22:04:19 +0200

    Add files via upload

---
+++ arnitikos_se_thetiko.py
@@ -0,0 +1,7 @@
+#Προγραμμα διψηφιος αριθμος
+arithmos=int(abs(input('Δώσε έναν θετικό αριθμό:')))
+if arithmos>=10 and arithmos<=99:
+    print 'Ο αριθμός',arithmos,'είναι διψήφιος'
+else:
+    print 'Ο αριθμός',arithmos,'δεν είναι διψήφιος'
+
